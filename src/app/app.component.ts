489454030d11a05be1fc9163f77e70f0207d9642
Harsh dave <harshdave1094@gmail.com>
2019-08-04 19:14:58 +0530

    MINOR CHANGE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Event Manager';
+  title = 'EVENT MANAGER';
   status = 'No events at the moment!'
 }
